85643b0c38795e6ea10832ff5c55e8bd27803303
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2022-01-17 16:58:46 -0500

    update inheritance

--- Potter/Session/Cookie/CookieSession.php
+++ Potter/Session/Cookie/CookieSession.php
@@ -2,12 +2,10 @@
 
 namespace Potter\Session\Cookie;
 
-use Potter\Session\Native\NativeSession;
-
-final class CookieSession extends NativeSession implements CookieSessionInterface
+final class CookieSession extends AbstractCookieSession
 {
     public function getCookie(): CookieInterface
     {
-        
+
     }
 }
