85f7b1d0ea5726ae6eecdb9ece848522ae2b5517
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2022-01-17 16:52:54 -0500

    make abstract and remove constructor

--- Potter/Session/Native/NativeSession.php
+++ Potter/Session/Native/NativeSession.php
@@ -4,16 +4,7 @@ namespace Potter\Session\Native;
 
 use Potter\Session\Session;
 
-final class NativeSession extends Session
+abstract class NativeSession extends AbstractNativeSession
 {
     use NativeSessionTrait;
-
-    public function __construct(string $name = null, bool $start = true)
-    {
-        if (isset($name))
-            $this->setName($name);
-        if (!$start)
-            return;
-        $this->start();
-    }
 }
