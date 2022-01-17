a1c0de901353d579b3bf50106ecd2ecdada3a99f
Jay Potter <34577317+jaypotter@users.noreply.github.com>
2022-01-17 16:54:56 -0500

    Create CookieSession.php

---
+++ Potter/Session/Cookie/CookieSession.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Potter\Session\Cookie;
+
+use Potter\Session\Native\NativeSession;
+
+final class CookieSession extends NativeSession implements CookieSessionInterface
+{
+    public function getCookie(): CookieInterface
+    {
+        
+    }
+}
